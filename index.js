31b394314c477cef7e8faa2af9bedb0a24805b51
Reduxirhealth <elvinneddall@gmail.com>
2025-05-18 14:27:22 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+{
+  "name": "reduxir-coachbot",
+  "version": "1.0.0",
+  "description": "Backend for Reduxir CoachBot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.3",
+    "openai": "^4.0.0"
+  },
+  "license": "MIT"
+}
